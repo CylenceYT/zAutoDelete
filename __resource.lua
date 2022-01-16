a3146c353b7b7adace1ee6b44d658453dd03dbce
Cylence <58346764+CylenceYT@users.noreply.github.com>
2022-01-16 02:59:33 -0500

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,9 +1,3 @@
---    CREDIT TO Whit3XLightning MOST OF THIS IS COPY/PASTE FROM HIS "DelAllVeh" SCRIPT
---                       I CODED SOME OF THIS BUT ITS MOSTLY HIS CODE.
---    THIS IS A FREE RELEASE SO PLEASE DONT STRESS TOO HARD. ITS JUST A PRANK BRO
---                                ANYWAY ENJOY <3
-
-
 client_scripts {
     'config.lua',
     'client/client.lua',
