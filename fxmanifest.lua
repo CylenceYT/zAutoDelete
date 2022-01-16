25c6d3d2bde780e56af28bd534d8372b2e0a907c
Cylence <58346764+CylenceYT@users.noreply.github.com>
2022-01-16 04:58:31 -0500

    Updated all my stuff on this...

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+
+game 'gta5'
+author 'Cylence'
+description 'zAutoDelete...It deletes vehicles after a certain time... what else should I say?'
+
+  client_scripts {
+      'config.lua',
+      'client/client.lua',
+      'client/entityiter.lua'
+  }
